539e5bf0f1cd6ab6335ecd3fde672e715ad9fed0
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-05 17:23:46 +0500

    разметка css

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ <div class="container">
+  <div class="card">
+   <h2> дерективы </h2>
+   <label for="int">Активный по умолчанию</label>
+   <div class="form-control">
+    <input type="text" id="int" >
+   </div>
+  </div>
+ </div>
 </template>
 
+<script setup>
+
+</script>
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
